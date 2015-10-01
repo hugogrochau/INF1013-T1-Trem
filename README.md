e2364d7113d2d7cb657eed84a5b4169e28b108e6
hugo@grochau.com <hugo@grochau.com>
2015-10-01 17:18:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# INF1013-T1-Trem 
+
+## Description
+Simulation of a train controlling system in Java
+
+## Authors
+* Hugo Grochau
+
+## License
+MIT
---
+++ src/pucrio/inf1013/t1/trem/Main.java
@@ -0,0 +1,8 @@
+package pucrio.inf1013.t1.trem;
+
+public class Main {
+	
+	public static void main(String args[]) {
+		System.out.println("PUC-RIO INF1013 T1 - Trem. By Hugo Grochau");
+	}
+}
